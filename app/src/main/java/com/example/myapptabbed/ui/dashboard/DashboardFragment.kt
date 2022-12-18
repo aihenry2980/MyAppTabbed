4224ef21a03d06f4eab595d9f76792d1bcc9d8a1
xinjinMBA <aihenry2980@gmail.com>
2022-12-18 12:17:44 +0900

    added bluetooth manager

--- app/src/main/java/com/example/myapptabbed/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapptabbed/ui/dashboard/DashboardFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myapptabbed.ui.dashboard
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapptabbed.databinding.FragmentDashboardBinding
@@ -32,6 +36,24 @@ class DashboardFragment : Fragment() {
 		dashboardViewModel.text.observe(viewLifecycleOwner) {
 			textView.text = it
 		}
+
+		val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+		val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+		if (bluetoothAdapter == null) {
+			// Device doesn't support Bluetooth
+
+		}
+		else{
+			if (bluetoothAdapter?.isEnabled == false) {
+				val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+			}
+		}
+
+
+
+
+
 		return root
 	}
 
